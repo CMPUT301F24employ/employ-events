00db37e87801eaee452e146ca104ccd5d18afeb3
tina-machi <tinale482@gmail.com>
2024-10-16 20:47:54 -0600

    Updated two navigation screens to be for facility and profile pages. Created facility and profile UI screen. Backend still needs to be done.

---
+++ app/src/main/java/com/example/employ_events/ui/profile/ProfileFragment.java
@@ -0,0 +1,36 @@
+package com.example.employ_events.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.employ_events.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProfileViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //final TextView textView = binding.textSlideshow;
+        //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
