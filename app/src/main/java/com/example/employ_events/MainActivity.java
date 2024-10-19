ff16f26ad9295054cbf12262f5ca24fc49cf5b0b
tina-machi <tinale482@gmail.com>
2024-10-18 21:59:26 -0600

    Created Profile class, adjusted notification switches on xmls, implemented create profile if new android device id for app and firebase. created firebase document "userProfiles" to store profile info with id as android device id. having trouble with the switches correctly showing the right notification setting.

--- app/src/main/java/com/example/employ_events/MainActivity.java
+++ app/src/main/java/com/example/employ_events/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.employ_events;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.Menu;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,11 +22,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.employ_events.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private String android_id;
+    private FirebaseFirestore db;
+    private CollectionReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +57,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        db = FirebaseFirestore.getInstance();
+
+        db.collection("userProfiles").document(android_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (!task.getResult().exists()) {
+                    Profile profile = new Profile(android_id);
+                    db.collection("userProfiles").add(profile);
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/employ_events/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/employ_events/ui/profile/ProfileFragment.java
@@ -1,28 +1,68 @@
 package com.example.employ_events.ui.profile;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.employ_events.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import android.provider.Settings.Secure;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private TextView name, email, phone_number;
+    private SwitchCompat organizer_notifications, admin_notifications;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel slideshowViewModel =
+        ProfileViewModel profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
+        String android_id = Secure.getString(requireContext().getContentResolver(), Secure.ANDROID_ID);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        organizer_notifications = binding.profileOrganizerNotificationStatus;
+        admin_notifications = binding.profileAdminNotificationStatus;
+        name = binding.profileName;
+        phone_number = binding.profilePhoneNumber;
+        email = binding.profileEmail;
+
+
+        DocumentReference profileRef = db.collection("userProfiles").document(android_id);
+        profileRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        organizer_notifications.setChecked(Boolean.TRUE.equals(document.getBoolean("organizerNotifications")));
+                        admin_notifications.setChecked(Boolean.TRUE.equals(document.getBoolean("adminNotifications")));
+                        name.setText(Objects.requireNonNull(document.get("name")).toString());
+                        email.setText(Objects.requireNonNull(document.get("email")).toString());
+                        phone_number.setText(Objects.requireNonNull(document.get("phoneNumber")).toString());
+                    }
+                }
+            }
+        });
         //final TextView textView = binding.textSlideshow;
         //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
