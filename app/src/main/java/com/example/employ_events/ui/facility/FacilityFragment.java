b210ea3133842b5c2b6d782cc79b10b323a45a8b
Aasvi-Kochhar <aasvi.kochhar@gmail.com>
2024-10-22 15:51:13 -0600

    Trying to push to aasvi branch

--- app/src/main/java/com/example/employ_events/ui/facility/FacilityFragment.java
+++ app/src/main/java/com/example/employ_events/ui/facility/FacilityFragment.java
@@ -4,27 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.employ_events.R;
 import com.example.employ_events.databinding.FragmentFacilityBinding;
 
 public class FacilityFragment extends Fragment {
 
     private FragmentFacilityBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        FacilityViewModel galleryViewModel =
+        FacilityViewModel facilityViewModel =
                 new ViewModelProvider(this).get(FacilityViewModel.class);
 
         binding = FragmentFacilityBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //final TextView textView = binding.textGallery;
-        //galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Set the button click listener to navigate to AddEventFragment
+       // binding.createEventButton.setOnClickListener(view -> Navigation.findNavController(view).navigate(R.id.action_facility_to_addEvent));
+
         return root;
     }
 
