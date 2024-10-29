6ca7248c784f7c316038ba0e7885391c41762237
“jasleeng16” <gilljasleen234@gmail.com>
2024-10-29 15:38:52 -0600

    commit id fix

--- app/src/main/java/com/example/employ_events/ui/registeredEvents/RegisteredArrayAdapter.java
+++ app/src/main/java/com/example/employ_events/ui/registeredEvents/RegisteredArrayAdapter.java
@@ -28,7 +28,6 @@ public class RegisteredArrayAdapter extends ArrayAdapter<Event> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-//        return super.getView(position, convertView, parent);
         View view = convertView;
 
         if(view == null){
