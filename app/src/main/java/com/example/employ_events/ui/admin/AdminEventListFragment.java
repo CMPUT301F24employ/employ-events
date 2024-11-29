1d8679667086a0da238e57421d051f33cf14882b
Connor Robinson <conjames11@gmail.com>
2024-11-29 10:55:01 -0700

    adding delete qr code button functionality, updating scanqrcode fragment so that event details page is only brought up if an event has its qr code image saved in firebase storage - nov 29

--- app/src/main/java/com/example/employ_events/ui/admin/AdminEventListFragment.java
+++ app/src/main/java/com/example/employ_events/ui/admin/AdminEventListFragment.java
@@ -22,16 +22,31 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 
+/**
+ * @author Connor
+ * AdminEventListFragment allows users who are admins (according to their firebase profile) to browse all the events
+ * stored in firebase. Admins are also able to click on an event to be able to delete the event or its qr code.
+ * US 03.04.01 As an administrator, I want to be able to browse events.
+ */
 public class AdminEventListFragment extends Fragment implements FacilityEventsAdapter.FEClickListener {
-
-    // for US 3.04.01 - browsing events
-    //
 
     private FragmentAdminEventListBinding binding;
     private FirebaseFirestore db;
     private ArrayList<Event> eventList;
     private FacilityEventsAdapter eventsAdapter;
 
+    /**
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @return the root view of the fragment
+     *
+     * Sets up the recycler view and queries & adds the events to the view.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAdminEventListBinding.inflate(inflater, container, false);
@@ -45,9 +60,11 @@ public class AdminEventListFragment extends Fragment implements FacilityEventsAd
         loadEvents();
 
         return root;
-
     }
 
+    /**
+     * Initializes the recycler view, sets the adapter to view the events, and adds dividers to make it look neat.
+     */
     private void setupRecyclerView() {
         RecyclerView recyclerView = binding.allEventsRecyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -55,6 +72,9 @@ public class AdminEventListFragment extends Fragment implements FacilityEventsAd
         recyclerView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
     }
 
+    /**
+     * Queries the events from firebase, adds them to the arraylist, and updates the recycler view
+     */
     private void loadEvents() {
         db.collection("events").get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
@@ -72,6 +92,11 @@ public class AdminEventListFragment extends Fragment implements FacilityEventsAd
         });
     }
 
+    /**
+     * @param event The event whose details will be displayed in the next fragment
+     * This method brings the admin to the manage event fragment where they will be able to delete the qr code or the entire event.
+     * @see com.example.employ_events.ui.events.ManageEventFragment
+     */
     @Override
     public void onItemClick(Event event) {
         // When you click on an event maybe handle other admin actions like deleting the event
